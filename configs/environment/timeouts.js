3efe03425ac11b409c3290f1255416f680a0f16e
Mikhail Pyshnik <74315389+mpushnikA1QA@users.noreply.github.com>
2022-06-28 14:40:15 +0300

    eslint config was added to the project (#3)

--- configs/environment/timeouts.js
+++ configs/environment/timeouts.js
@@ -1,8 +1,8 @@
 module.exports = {
-    polling: 500,
-    explicit: 70000,
-    implicit: 0,
-    pageLoadTime: 12000,
-    cucumberStep: 30000,
-    apiRequest: 5000,
-  };
+  polling: 500,
+  explicit: 70000,
+  implicit: 0,
+  pageLoadTime: 12000,
+  cucumberStep: 30000,
+  apiRequest: 5000,
+};
--- src/testData/schemes/meta.js
+++ src/testData/schemes/meta.js
@@ -1,30 +1,30 @@
 const jsonSchemaForMetaSection = {
-    type: "object",
-    properties: {
-        meta: {
-        type: "object",
-        properties: {
-            statusCode:  { type: 'number' },
-            message: { type: 'string' },
-            version: { type: 'string' },
-            schemaVersion: { type: 'string' },
-            generated: { type: 'string' },
-            queryUrl: { type: 'string' },
-        },
-        required: [
-            'statusCode', 
-            'message', 
-            'version',
-            'schemaVersion',
-            'generated',
-            'queryUrl'
-          ],additionalProperties: false
-        },
+  type: "object",
+  properties: {
+    meta: {
+      type: "object",
+      properties: {
+        statusCode:  { type: 'number' },
+        message: { type: 'string' },
+        version: { type: 'string' },
+        schemaVersion: { type: 'string' },
+        generated: { type: 'string' },
+        queryUrl: { type: 'string' },
+      },
+      required: [
+        'statusCode', 
+        'message', 
+        'version',
+        'schemaVersion',
+        'generated',
+        'queryUrl'
+      ],additionalProperties: false
     },
-    required: ["meta"],
-    additionalProperties: true
-}
+  },
+  required: ["meta"],
+  additionalProperties: true
+};
 
 module.exports = {
-    jsonSchemaForMetaSection
-  };
+  jsonSchemaForMetaSection
+};
